9587bcb2c17dc15ae277dd1d11d230e244172f33
mj-meyer <hello@mjmeyer.dev>
2024-08-10 01:32:35 +1200

    ⬆ Upgrade to Astro v4

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+     env: {
+       node: true,
+       es2022: true,
+       browser: true,
+     },
+     extends: [
+       "eslint:recommended",
+       "plugin:@typescript-eslint/recommended",
+       "plugin:astro/recommended",
+     ],
+     parser: "@typescript-eslint/parser",
+     plugins: ["@typescript-eslint"],
+     parserOptions: {
+       ecmaVersion: "latest",
+       sourceType: "module",
+       project: "./tsconfig.json",
+     },
+     overrides: [
+       {
+         files: ["*.astro"],
+         parser: "astro-eslint-parser",
+         parserOptions: {
+           parser: "@typescript-eslint/parser",
+           extraFileExtensions: [".astro"],
+         },
+         rules: {},
+       },
+       {
+         files: ["*.ts", "*.tsx"],
+         parser: "@typescript-eslint/parser",
+         extends: ["plugin:@typescript-eslint/recommended"],
+       },
+     ],
+   };
--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -3,9 +3,14 @@ import generateOgImage from "@utils/generateOgImage";
 import type { APIRoute } from "astro";
 import { slugifyStr } from "@utils/slugify";
 
-export const get: APIRoute = async ({ params, props }) => ({
-  body: await generateOgImage(params.ogTitle, props.postTitle),
-});
+export const get: APIRoute = async ({ params, props }) => {
+  const body = await generateOgImage(params.ogTitle, props.postTitle);
+  return new Response(body, {
+    headers: {
+      "Content-Type": "image/png", // Adjust this if your image is not a PNG
+    },
+  });
+};
 
 const postImportResult = await getCollection("blog", ({ data }) => !data.draft);
 const posts = Object.values(postImportResult);
