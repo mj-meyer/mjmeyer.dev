3ddd93a90a80a42844ff0211dc16e506bc9884b1
mj-meyer <hello@mjmeyer.dev>
2024-09-29 18:16:53 +1300

    ✨ add topics to replace tags

--- src/utils/getPostsByTag.ts
+++ src/utils/getPostsByTag.ts
@@ -1,6 +1,12 @@
 import type { CollectionEntry } from "astro:content";
+import { normalizeTag } from "./tagUtils";
 
-const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-  posts.filter(post => post.data.tags.includes(tag));
+const getPostsByTag = (
+  posts: CollectionEntry<"garden" | "stream">[],
+  tag: string
+) =>
+  posts.filter(post =>
+    post.data.tags.some(postTag => normalizeTag(postTag) === normalizeTag(tag))
+  );
 
 export default getPostsByTag;
